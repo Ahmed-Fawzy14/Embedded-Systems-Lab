54af53c6351bacb98619c4c195d1fff0549a3d17
Ahmed Fawzy <124689355+Ahmed-Fawzy14@users.noreply.github.com>
2026-03-26 23:18:08 +0200

    Refactor UART0 setup and LED control
    Refactor UART0 initialization and interrupt handling. Remove unused UART1 setup and improve LED control.

--- q1.c
+++ q1.c
@@ -1,68 +1,69 @@
-#include <stdint.h>
+/* Read data from UART0 and display it at the tri-color LEDs. The LEDs are
+connected to Port F 3-1. Press any A-z, a-z, 0-9 key at the terminal emulator
+and see ASCII value in binary is displayed on LEDs of PORTF. */
 #include "tm4c123gh6pm.h"
-void UART0Tx(char const c);
+#include<stdio.h>
 void delayMs(int n);
+
+volatile char yes[] = "yes ";
+volatile int i = 0;
 int main(void)
 {
 SYSCTL->RCGCUART |= 1; /* provide clock to UART0 */
 SYSCTL->RCGCGPIO |= 1; /* enable clock to PORTA */
+SYSCTL->RCGCGPIO |= 0x20; /* enable clock to PORTF */
 /* UART0 initialization */
 UART0->CTL = 0; /* disable UART0 */
 UART0->IBRD = 104; /* 16MHz/(16*9600 baud rate) = 104.1666666666 */
-UART0->FBRD = 11; /* fraction part = 0.1666666*64+0.5 = 11.1666666 */
+UART0->FBRD = 11; /* fraction part= 0.1666666*64+0.5 = 11.1666666 */
 UART0->CC = 0; /* use system clock */
 UART0->LCRH = 0x60; /* 8-bit, no parity, 1-stop bit, no FIFO */
-UART0->CTL = 0x301; /* enable UART0(bit0), TXE(bit8), RXE(bit9) */
+UART0->IM |= 0x20; /* enable TX interrupt */
+UART0->CTL = 0x301; /* enable UART0, TXE, RXE */
 /* UART0 TX0 and RX0 use PA1 and PA0. Set them up. */
 GPIOA->DEN = 0x03; /* Make PA0 and PA1 as digital */
 GPIOA->AFSEL = 0x03; /* Use PA0,PA1 alternate function */
 GPIOA->PCTL = 0x11; /* configure PA0 and PA1 for UART */
-//delayMs(25); /* wait for output line to stabilize */
-
-/* UART1 TX0 and RX0 use PB1 and PB0. Set them up. */
-GPIOB->DEN |= 0x03; /* Make PB0 and PB1 as digital */
-GPIOB->AFSEL |= 0x03; /* Use PB0,PB1 alternate function */
-GPIOB->PCTL &= ~0x000000FF;
-GPIOB->PCTL |= 0x00000011; /* configure PB0 and PB1 for UART */
-
-/* configure Port F pins 3,2,1 to control the LEDs */
 GPIOF->DIR = 0x0E; /* configure Port F to control the LEDs */
 GPIOF->DEN = 0x0E;
 GPIOF->DATA = 0; /* turn LEDs off */
-
-/* enable UART1 interrupt in NVIC and set priority to 3 */
-
-// Since this is IP[6] so we need to enable the 6th bit in ISER
-NVIC->IP[6] = 3 << 5; /* set interrupt no 6 priority to 3 */
-NVIC->ISER[0] |= 0x00000040; /* enable IRQ6 for UART1 */
-
+/* enable interrupt in NVIC and set priority to 3 */
+NVIC->IP[5] = 3 << 5; /* set interrupt no 5 priority to 3 */
+NVIC->ISER[0] |= 0x00000020; /* enable IRQ5 for UART0 */
 __enable_irq(); /* global enable IRQs */
+
+delayMs(100); /* wait for output line to stabilize */
+
+
+UART0->DR = yes[i++]; // Write first one
+
+if(yes[i] == '\0')
+	i = 0;
 	
 
-while(1)
-{
+
+for(;;){}
 }
-}
-/* UART0 Transmit */
-/* This function waits until the transmit buffer is available then writes */
-/* the character in it. It does not wait for transmission to complete */
-void UART0Tx(char const c)
-{
-	while((UART0->FR & 0x20)!= 0){} // Wait until Tx buffer is not full
-	UART0->DR = c; // Write byte
-}
+
 
 void UART0_Handler(void)
 {
-volatile int readback;
-char c;
-	if (UART0->MIS & 0x0010) /* if a receive interrupt has occurred */
-	{
-		UART0Tx('Y');
-		UART0Tx('e');
-		UART0Tx('s');
-		UART0Tx(' ');
-		UART0->ICR = 0x0010; /* clear Rx interrupt flag */
-		readback = UART0->ICR; /* a read to force clearing of interrupt flag */
-	} 
+if (UART0->MIS & 0x20) /* if a receive interrupt has occurred */
+{
+
+	UART0->DR = yes[i++]; // Write char
+	
+	if(yes[i] == '\0')
+		i = 0;
+
+	UART0->ICR = 0x20; /* clear Rx interrupt flag */
+} 
+}
+
+void delayMs(int n)
+{
+int i, j;
+for(i = 0 ; i < n; i++)
+for(j = 0; j < 3180; j++)
+{} // do nothing for 1 ms
 }
